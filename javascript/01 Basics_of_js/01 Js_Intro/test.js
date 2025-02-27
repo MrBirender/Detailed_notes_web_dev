5f383a2ab5afed353a5a2f56fc1d8059db36baad
MrBirender <birenderyadav0918@gmail.com>
2025-02-27 12:55:21 +0530

    NOTES UPDATED

--- javascript/01 Basics_of_js/01 Js_Intro/test.js
+++ javascript/01 Basics_of_js/01 Js_Intro/test.js
@@ -1,8 +1,89 @@
-let name = 'birender' // PRIMITIVE
-let newName = name
+/* arithmatic operaions with primitivs */
 
- consol
+console.log('5' + 5)
+console.log(true + 5)
+console.log(undefined + 5)// undefined can't be coerced
+console.log(null + 5)
+// console.log(Symbol + 5)
+// console.log(BigInt + 5)
+console.log(5 + 5)
 
-let obj = {name: 'maya', age: ' 27'} //NON-PRIMITIVE
+console.log('secnod part') 
 
-let newObj = obj
+console.log('5' * 5)
+console.log(true * 5)
+console.log(undefined * 5)
+console.log(null* 5)
+
+
+console.log('third part') 
+
+console.log('5' / 5)
+console.log(true / 5)
+console.log(undefined / 5)
+console.log(null/ 5)
+
+
+console.log('fourth part') 
+
+console.log('5' - 5)
+console.log(true - 5)
+console.log(undefined - 5)
+console.log(null- 5)
+
+/* in arithmatice operaiton null is coered to zero */
+
+/* comparsions with primitives */
+
+console.log('5th part')
+
+console.log('5' == 5)
+console.log('1' == true)
+console.log('1' == undefined)
+
+/* null is coerced to zero but when cheking equality they are not equal to any one because they have seperate rules. and theya re only equal to each other */
+console.log(null == 0) // doubt alert
+console.log(null == undefined) // doubt alert
+
+
+/* comparisos with non-primitive data types. */
+
+console.log('non-primitives')
+console.log([] + '5')
+console.log({}- "5")
+console.log({} - 5)
+console.log({} * '5')
+console.log({}/"5")
+
+/* we can't perform mathematical operaition with primitve and non-primitive */
+
+
+console.log('non-primitives')
+console.log([] == '0')
+console.log([] == 0)
+const hello = () => {}
+console.log(hello == 0)
+/* 
+[] converts to "" (empty string) because .toString() on an array returns a string.
+{} converts to "[object Object]" because objects have no default meaningful primitive conversion. */
+
+console.log(0 === -0);
+
+const s ={}.valueOf()
+console.log( s.toString())
+
+
+/* [].toString() → "" → 0
+{}.toString() → "[object Object]" → NaN */
+
+
+/* In browsers, {} alone is treated as a block of code.
+In Node.js, {} is interpreted as an object, so it follows JavaScript's coercion rules.
+To compare an object in the browser console, use parentheses: ({} == 0).
+Objects do not coerce to numbers, so {} == 0 is always false.
+Hope this clears things up! 🚀 Let me know if you have any more doubts. 😃 */
+console.log({}.valueOf())
+
+
+console.log({} == {})
+console.log({} === {})
