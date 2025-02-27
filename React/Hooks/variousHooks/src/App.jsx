0a3d9077f20a3a6d2ea4c492efb62a7fc108a387
MrBirender <birenderyadav0918@gmail.com>
2025-02-27 09:45:30 +0530

    useReducer hook

--- React/Hooks/variousHooks/src/App.jsx
+++ React/Hooks/variousHooks/src/App.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import Profile from "./components/Profile";
-import Footer from "./components/Footer";
+import { useReducer } from "react";
 
 const App = () => {
+  const initialState = { count: 0 };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return { count: state.count + 1 };
+        
+      case "DECREMENT":
+        return { count: state.count - 1 };
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="absolute h-full flex flex-col gap-12 items-center justify-center w-full bg-black   text-white text-2xl">
-      <Profile />
-      <Footer />
+      <p>count: {state.count} </p>
+      <button
+        onClick={() => dispatch({ type: "INCREMENT" })}
+        className="px-4 py-2 mt-6 border bg-black rounded-lg"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => dispatch({ type: "DECREMENT" })}
+        className="px-4 py-2 mt-6 border bg-black rounded-lg"
+      >
+        Decrement
+      </button>
     </div>
   );
 };
