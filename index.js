3f4513fcb1a402a5995d88f3b0b9c38b1be25589
MrBirender <birenderyadav0918@gmail.com>
2025-03-24 16:01:03 +0530

    delete small projects

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-function runProgram(input) {
-  // Write code here
-  const lines = input.trim().split("\n");
 
-  const t = parseInt(lines[0]);
-  let index = 1;
+const str = 'toMe'
 
-  for (let i = 0; i < t; i++) {
-    const length = lines[index].trim();
-    const string = lines[index + 1].trim();
-
-    index += 2;
-
-    /*sum*/
-    let sum = parseInt(string[0]);
-
-    for (let j = 1; j < length; j++) {
-      if (string[j - 1] === "1" && string[j] === "1") {
-        sum -= parseInt(string[j]);
-      } else {
-        sum += parseInt(string[j]);
-      }
-    }
-
-    console.log(Math.abs(sum));
-  }
-}
-
-let input = `2
-  4
-  1101
-  7
-  0110111`;
-runProgram(input);
+console.log(/[rmxc]/i.test(str))
--- javascript/01 Basics_of_js/06 Topics- important/Destructuring.js
+++ javascript/01 Basics_of_js/06 Topics- important/Destructuring.js
@@ -1,4 +1,155 @@
-
 // Extract the first, third (with a default value of 'Orange' if it’s undefined), and the last fruit. Skip the second fruit.
 
-const fruits = ['Apple', 'Banana', undefined, 'Mango', 'Pineapple'];
+const fruits = ["Apple", "Banana", undefined, "Mango", "Pineapple"];
+
+const [first, , third = "orange", , last] = fruits;
+
+// console.log(first, third, last)
+
+/* i can use comma to skip the destrutring to skip a particular item  */
+
+// Destructure the name and location properties, but rename them to userName and userLocation.
+const user = {
+  id: 101,
+  name: "John Doe",
+  age: 28,
+  location: "New York",
+};
+
+const { id, location: mylocation, name: userName, age } = user;
+
+// console.log(id, userName, age, mylocation);
+/*    Extract title, brand, ram, and storage using nested destructuring.
+ */
+
+const product = {
+  ide: 1,
+  title: "Laptop",
+  details: {
+    brand: "Dell",
+    specs: {
+      ram: "16GB",
+      storage: "512GB SSD",
+    },
+  },
+};
+
+const {
+  ide,
+  title,
+  details: {
+    brand,
+    specs: { ram, storage },
+  },
+} = product;
+
+//   console.log(title, brand, ram, storage);
+
+/*   Destructure theme and language, with a default value of false for notifications. Use the rest operator to gather any remaining properties. */
+
+const settings = {
+  theme: "dark",
+  language: "English",
+  notifications: true,
+  birender: "birender",
+};
+
+const { theme, language, notifications = false, ...otherSettings } = settings;
+
+console.log(theme, language, notifications, otherSettings);
+
+/* in place of rest we can give ...anyName  */
+
+// Destructure the first two numbers and gather the rest of the numbers in a new array called remainingNumbers.
+
+const numbers = [10, 20, 30, 40, 50];
+
+const [firstva, second, ...remainingNumbers] = numbers;
+
+console.log(firstva, second, remainingNumbers);
+
+/* 6. Function Parameter Destructuring (Object)
+Question:
+Write a function displayBookInfo that accepts an object as an argument with properties: title, author, and year. Destructure these properties inside the function and log them. Provide a default value for year if it’s not passed. */
+
+function displayBookInfo({ title, author, year = "N/A" }) {
+  console.log(title, author, year);
+}
+
+const obj = {
+  title: 1,
+  author: 2,
+};
+
+displayBookInfo(obj);
+
+/*;
+Use nested array destructuring to extract red, green, and yellow. */
+const colors = [
+  ["red", "green"],
+  ["blue", "yellow"],
+];
+
+const [[fc, sc], [, tc]] = colors;
+console.log(fc, sc, tc);
+
+// Destructure the array to extract the name of the second employee and the department of the first employee.
+
+const employees = [
+  { name1: "Alice", department: "HR" },
+  { name1: "Bob", department: "Engineering" },
+];
+
+const [{ department }, { name1 }] = employees;
+console.log(name1, department);
+
+/* swapping problem */
+/* Question:
+Without using a temporary variable, swap the values of a and b: */
+
+let a = 5;
+let b = 10;
+
+// swapping these with temp
+
+let temp = a;
+a = b;
+b = temp;
+
+/* using destructuring */
+
+let z = 14;
+let y = 15;
+
+/* some time js automatic asi automatci semicolon insertion can malfucntion and can give errors like ; 
+
+Uncaught ReferenceError: Cannot access 'y' before initialization
+    at Destructuring.js:131:5*/
+
+[z, y] = [y, z];
+
+console.log(z, y);
+
+/*   Destructure name, theme, and language (with a default value of 'English') from userData while safely handling cases where preferences or language may be missing.
+ */
+
+const userData = {
+  profile: {
+    name: "Jane",
+   
+  },
+};
+
+const {
+  profile: {
+    name,
+    age2,
+    preferences: { theme1, language1 = "English" } = {},
+  },
+} = userData;
+
+/* it works like that if value is missing then we can provied the empty obj that will prevent the code to break */
+
+console.log(name, age2, theme1, language1);
+
+/* clear this block scope error. can't redeclare block scope. */
