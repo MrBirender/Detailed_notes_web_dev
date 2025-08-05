dde96bac772d7565f6bf78633a81c259476e4975
MrBirender <birenderyadav0918@gmail.com>
2025-08-05 10:14:25 +0530

    chane index.ts file

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,76 +1,31 @@
-
-interface User {
-    name: string;
-    age: number;
+function add(num1: number, num2: number):number {
+  return num1 + num2;
 }
 
-let person: User = {
-    name: "John",
-    age: 25
+console.log(add(2, 5));
+
+// type aliases
+
+type userData = {
+  name:string
+  age:number
+  cordinates:point
+
 }
 
-/* type inference */
-let person1 = 'sonu';
-
-/* person1 = 25 */;/* in starting i have declared person1 as string
-and without even stating the type ts knows that it can't be other 
-then a string. it is called type inference */
-
-
-/* diff between any, unknown and never */
-let dog: any = 'maxu';
-dog = 25;
-/* it is used to aviod type checking, so it can be any data types
-but it should be avoided. */
-
-let cat: unknown = 'billi';
-/* it is similart to the any but needs type check to perform an action.
- */
-if(typeof cat === 'string'){
-    console.log(cat.toUpperCase());
+type point = {
+  x: number,
+  y: number
 }
 
-let rabbit: never;
-/* never is used to represent a value that is never observed.
-it is used to represent the type of a function that never returns
-a value.
- */
-/* a real life coding example for thi would be */
-
-function error(message: string): never {
-    throw new Error(message);
-   
+function giveCordinates (pt: point):userData{
+return  {name:'birender', age:3, cordinates:pt}
 }
 
-/* default and optional parameters */
-function user(name?: string, age?: number){
-console.log(`Hai ${name || 'guest'} age is ${age || 'unknown'}`);
+console.log(giveCordinates({x:14.44, y:55.88}))
+
+// combining types restrict editing
+
+type cardNumber = {
+   cardNumber :"number"
 }
-
-/* default parameters */
-function user1(name:string = 'guest', age:number = 25){
-    console.log(`Hai ${name} age is ${age}`);
-}
-
-user();
-user1();
-
-
-/* how to type props and states in react fc */
-
-/* import React from 'react
-
-interface Props {
-name: string,
-age: number
-}
-
-const App: React.FC<Props> = ({name, age}) => {
-return (
-    <div>
-        <h1>hello</h1>
-    </div>
-)}
-*/
-
-/* const [count, setCount] = useState<number>(0) */
